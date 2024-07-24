f5a47fb9183ca35148ae334a69f8e25767288253
Young1 <dlduddnjs198@naver.com>
2024-07-24 11:26:48 +0900

    add realtime

---
+++ backend-realtime/src/main/java/com/plog/realtime/BackendRealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.plog.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRealtimeApplication.class, args);
+    }
+
+}
---
+++ backend-realtime/src/test/java/com/plog/realtime/BackendRealtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plog.realtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRealtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
