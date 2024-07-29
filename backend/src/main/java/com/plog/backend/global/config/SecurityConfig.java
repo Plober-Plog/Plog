7cb5891c5f9a6350d32509d09f27fa5a24eb7f93
강윤서 <ysys1130@gmail.com>
2024-07-29 19:07:22 +0900

    chore: Modify build.gradle and other configuration files

--- backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
@@ -1,4 +1,64 @@
 package com.plog.backend.global.config;
 
+import com.plog.backend.domain.user.service.UserService;
+import com.plog.backend.global.auth.PloberUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private PloberUserDetailService PloberUserDetailService;
+
+    @Autowired
+    private UserService userService;
+
+    // Password 인코딩 방식에 BCrypt 암호화 방식 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // DAO 기반으로 Authentication Provider를 생성
+    // BCrypt Password Encoder와 UserDetailService 구현체를 설정
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(PloberUserDetailService);
+        return daoAuthenticationProvider;
+    }
+
+    // AuthenticationManager 빈 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // SecurityFilterChain 빈 등록
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable);
+//            .oauth2Login(Customizer.withDefaults());
+        return http.build();
+
+
+    }
 }
