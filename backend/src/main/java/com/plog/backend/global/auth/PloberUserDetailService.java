bd21f5b95f000a3c1550386e12a882ae58d74402
강윤서 <ysys1130@gmail.com>
2024-07-29 19:09:43 +0900

    feat: Add PloberUserDetailService and PloberUserDetails classes

--- backend/src/main/java/com/plog/backend/global/auth/PloberUserDetailService.java
+++ backend/src/main/java/com/plog/backend/global/auth/PloberUserDetailService.java
@@ -1,2 +1,25 @@
-package com.plog.backend.global.auth;public class PloberUserDetailService {
+package com.plog.backend.global.auth;
+
+import com.plog.backend.domain.user.entity.User;
+import com.plog.backend.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PloberUserDetailService implements UserDetailsService {
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserBySearchId(username);
+        if(user != null) {
+            PloberUserDetails userDetails = new PloberUserDetails(user);
+            return userDetails;
+        }
+        return null;
+    }
 }
--- backend/src/main/java/com/plog/backend/global/auth/PloberUserDetails.java
+++ backend/src/main/java/com/plog/backend/global/auth/PloberUserDetails.java
@@ -1,2 +1,57 @@
-package com.plog.backend.global.auth;public class PloberUserDetails {
+package com.plog.backend.global.auth;
+
+import com.plog.backend.domain.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class PloberUserDetails implements UserDetails {
+    @Autowired
+    User user;
+    boolean accountNonExpired;
+    boolean accountNonLocked;
+    boolean credentialNonExpired;
+    boolean enabled = false;
+    List<GrantedAuthority> roles = new ArrayList<>();
+
+    public PloberUserDetails(User user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+    @Override
+    public String getUsername() {
+        return this.user.getSearchId();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialNonExpired;
+    }
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+    public void setAuthorities(List<GrantedAuthority> roles) {
+        this.roles = roles;
+    }
 }
