35bbd250705ee0c76ec73b4975b552e138275fe2
강윤서 <ysys1130@gmail.com>
2024-07-31 00:54:48 +0900

    feat: image 추íupu: uImage 구현

--- backend/src/main/java/com/plog/backend/domain/image/service/ImageServiceImpl.java
+++ backend/src/main/java/com/plog/backend/domain/image/service/ImageServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service("imageService")
 public class ImageServiceImpl implements ImageService {
+
     @Autowired
     private ImageRepository imageRepository;
 
