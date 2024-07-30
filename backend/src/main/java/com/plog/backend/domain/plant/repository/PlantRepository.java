3896c425c4ac489442b6db14c15304830921aca4
강윤서 <ysys1130@gmail.com>
2024-07-31 00:57:29 +0900

    feat: Plant J ãPlant Repository 생성

--- backend/src/main/java/com/plog/backend/domain/plant/repository/PlantRepository.java
+++ backend/src/main/java/com/plog/backend/domain/plant/repository/PlantRepository.java
@@ -4,9 +4,6 @@ import com.plog.backend.domain.plant.entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PlantRepository  extends JpaRepository<Plant, Long> {
-    Optional<Plant> addPlant(Plant plant);
 }
--- backend/src/main/java/com/plog/backend/domain/plant/repository/PlantRepositorySupport.java
+++ backend/src/main/java/com/plog/backend/domain/plant/repository/PlantRepositorySupport.java
@@ -1,18 +1,8 @@
 package com.plog.backend.domain.plant.repository;
 
-import com.plog.backend.domain.plant.entity.Plant;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PlantRepositorySupport {
-    @Autowired
-    private JPAQueryFactory jpaQueryFactory;
-    @Autowired
-    private PlantRepository plantRepository;
 
-    public Plant addPlant(Plant plant) {
-        return plantRepository.save(plant);
-    }
 }
