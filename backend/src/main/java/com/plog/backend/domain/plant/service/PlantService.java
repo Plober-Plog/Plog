f48257bfdf151940bf88060082fd8b533176eed7
강윤서 <ysys1130@gmail.com>
2024-07-31 11:16:42 +0900

    feat: controller test 실패 ì¼ 케이스 추가 및 TODO 작성

--- backend/src/main/java/com/plog/backend/domain/plant/service/PlantService.java
+++ backend/src/main/java/com/plog/backend/domain/plant/service/PlantService.java
@@ -6,4 +6,8 @@ import com.plog.backend.domain.plant.exception.NotValidPlantTypeIdsException;
 
 public interface PlantService {
     Plant addPlant(PlantAddRequest plantAddRequest) throws NotValidPlantTypeIdsException;
+
+    //TODO
+    // - 1. isFixed(boolean) 을 fixed(int) 로 변환하는 로직
+    // - 2. waterDate, fertilizeDate, repotDate 계산하는 로직
 }
--- backend/src/test/java/com/plog/backend/controller/PlantControllerTest.java
+++ backend/src/test/java/com/plog/backend/controller/PlantControllerTest.java
@@ -6,10 +6,11 @@ import com.plog.backend.domain.plant.dto.request.PlantAddRequest;
 import com.plog.backend.domain.plant.entity.Plant;
 import com.plog.backend.domain.plant.service.PlantService;
 import com.plog.backend.global.model.response.BaseResponseBody;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,16 +18,21 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PlantController.class)
 @AutoConfigureMockMvc
 public class PlantControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -36,29 +42,105 @@ public class PlantControllerTest {
     @MockBean
     private PlantService plantService;
 
-    @Test
-    @WithMockUser(username = "user", roles = {"USER"})
+    @Nested
     @DisplayName("식물 등록")
-    void canAddPlant() throws Exception {
-        // given
-        PlantAddRequest requestDto = new PlantAddRequest();
-        Plant plant = new Plant();
+    class AddPlant {
 
-        // Mocking the service method
-        when(plantService.addPlant(any(PlantAddRequest.class))).thenReturn(plant);
+        PlantAddRequest plantAddRequest;
 
-        // expected response body
-        BaseResponseBody expectedResponse = BaseResponseBody.of(200, "식물 등록이 완료되었습니다.");
+        @Nested
+        @DisplayName("정상 케이스")
+        class SuccessCase {
 
-        // when
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/user/plant")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestDto))
-                        .with(csrf()))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(expectedResponse)));
+            @BeforeEach
+            void setUp() throws ParseException {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date birthDate = dateFormat.parse("2024-07-30");
+                plantAddRequest = new PlantAddRequest().builder()
+                        .profile("imageUrl")
+                        .nickname("nickname")
+                        .birthDate(birthDate)
+                        .hasNotified(false)
+                        .isFixed(false).build();
+            }
 
-        // then
-        verify(plantService).addPlant(any(PlantAddRequest.class));  // Verify the service method was called
+            @Test
+            @WithMockUser(username = "user", roles = {"USER"})
+            @DisplayName("유효한 요청으로 등록 성공")
+            void addPlantSuccessfully() throws Exception {
+                // given
+                Plant plant = new Plant();
+                when(plantService.addPlant(any(PlantAddRequest.class))).thenReturn(plant);
+                BaseResponseBody expectedResponse = BaseResponseBody.of(200, "식물 등록이 완료되었습니다.");
+
+                // when - then
+                mockMvc.perform(MockMvcRequestBuilders.post("/api/user/plant")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(plantAddRequest))
+                                .with(csrf()))
+                        .andExpect(status().isOk())
+                        .andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)));
+
+                verify(plantService).addPlant(any(PlantAddRequest.class)); // service method 호출 확인
+            }
+        }
+
+        @Nested
+        @DisplayName("비정상 케이스")
+        class FailureCase {
+
+            @BeforeEach
+            void setUp() throws ParseException {
+                plantAddRequest = new PlantAddRequest().builder()
+                        .profile("imageUrl")
+                        .hasNotified(false)
+                        .isFixed(false).build();
+            }
+
+            //TODO
+            // - 권한이 없는 회원의 요청에 대한 401 예외 테스트
+
+            @Test
+            @WithMockUser(username = "user", roles = {"USER"})
+            @DisplayName("닉네임 누락으로 등록 실패")
+            void failWhenNicknameIsMissing() throws Exception {
+                // given
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date birthDate = dateFormat.parse("2024-07-30");
+                plantAddRequest.setBirthDate(birthDate);
+
+                // when - then
+                mockMvc.perform(MockMvcRequestBuilders.post("/api/user/plant")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(plantAddRequest))
+                                .with(csrf()))
+                        .andExpect(status().isBadRequest())
+                        .andExpect(jsonPath("$.httpMethod").value("POST"))
+                        .andExpect(jsonPath("$.requestURL").value("/api/user/plant"))
+                        .andExpect(jsonPath("$.httpStatus").value(400))
+                        .andExpect(jsonPath("$.message").value("nickname 은 필수 필드입니다."))
+                        .andExpect(jsonPath("$.timestamp").exists());
+            }
+
+            @Test
+            @WithMockUser(username = "user", roles = {"USER"})
+            @DisplayName("생일 누락으로 등록 실패")
+            void failWhenBirthDateIsMissing() throws Exception {
+                // given
+                plantAddRequest.setNickname("nickname");
+
+                // when - then
+                mockMvc.perform(MockMvcRequestBuilders.post("/api/user/plant")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(plantAddRequest))
+                                .with(csrf()))
+                        .andExpect(status().isBadRequest())
+                        .andExpect(jsonPath("$.httpMethod").value("POST"))
+                        .andExpect(jsonPath("$.requestURL").value("/api/user/plant"))
+                        .andExpect(jsonPath("$.httpStatus").value(400))
+                        .andExpect(jsonPath("$.message").value("birthDate 는 필수 필드입니다."))
+                        .andExpect(jsonPath("$.timestamp").exists());
+            }
+        }
     }
 }
