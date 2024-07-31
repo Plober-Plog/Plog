842f30c2d90a6772165f655a0b14c097b15a82b2
강윤서 <ysys1130@gmail.com>
2024-07-31 09:26:13 +0900

    feat: PlantExceptionHandler 추가

--- backend/src/main/java/com/plog/backend/domain/plant/exception/handler/PlantExceptionHandler.java
+++ backend/src/main/java/com/plog/backend/domain/plant/exception/handler/PlantExceptionHandler.java
@@ -1,2 +1,26 @@
-package com.plog.backend.domain.plant.exception.handler;public class Handler {
+package com.plog.backend.domain.plant.exception.handler;
+
+import com.plog.backend.domain.plant.exception.NotValidPlantTypeIdsException;
+import com.plog.backend.global.exception.ExceptionResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class PlantExceptionHandler {
+    @ExceptionHandler(NotValidPlantTypeIdsException.class)
+    public ResponseEntity<ExceptionResponseDto> handleConflictingPlantTypeIdsException(
+            NotValidPlantTypeIdsException ex, HttpServletRequest request) {
+
+        ExceptionResponseDto response = ExceptionResponseDto.of(
+                request.getMethod(),
+                request.getRequestURI(),
+                HttpStatus.BAD_REQUEST.value(),
+                "유효한 식물 종류가 아닙니다."
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
