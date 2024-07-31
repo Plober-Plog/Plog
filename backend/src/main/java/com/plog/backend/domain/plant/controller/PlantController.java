3f6434ca3cd37bce5735da2c5f672da77dbec43c
강윤서 <ysys1130@gmail.com>
2024-07-31 17:51:40 +0900

    refactor: base URI 설정 - /api

--- backend/src/main/java/com/plog/backend/domain/plant/controller/PlantController.java
+++ backend/src/main/java/com/plog/backend/domain/plant/controller/PlantController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/plant")
+@RequestMapping("/user/plant")
 public class PlantController {
 
     @Autowired
