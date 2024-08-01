ed47c28630807697032b26abf49b84ada6ecdf86
gapple95 <gapple95@naver.com>
2024-08-01 13:06:20 +0900

    fix: 테스트 코드 수정 및 User 테이블 명 변경

--- backend/src/main/java/com/plog/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/plog/backend/domain/user/entity/User.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Getter
