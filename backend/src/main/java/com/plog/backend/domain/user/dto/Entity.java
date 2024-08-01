e507b5c68788c1b7647d56026c4d7ca69f6f6c0c
강윤서 <ysys1130@gmail.com>
2024-08-02 00:59:41 +0900

    chore: validation 추가

--- backend/src/main/java/com/plog/backend/domain/user/dto/Entity.java
+++ backend/src/main/java/com/plog/backend/domain/user/dto/Entity.java
@@ -1,10 +1,8 @@
 package com.plog.backend.domain.user.dto;
 
-import com.plog.backend.domain.user.entity.*;
 import com.plog.backend.global.model.dto.BaseRequestDto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.Date;
@@ -20,7 +18,6 @@ public class RequestSignUpDto extends BaseRequestDto {
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "유효한 이메일 형식이 아닙니다.")
     private String email;
-
 
     @NotBlank(message = "검색 ID는 필수 입력 값입니다.")
     private String searchId;
