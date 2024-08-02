8b6dd64e76cb0a748cba21e52320086084fc47f8
gapple95 <gapple95@naver.com>
2024-08-02 13:05:59 +0900

    fix: redis config에서 비밀번호를 설정했습니다.

--- backend/src/main/java/com/plog/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/plog/backend/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -15,9 +17,16 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        redisConfig.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(redisConfig);
     }
 
     @Bean
