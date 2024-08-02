bdf30c7277e9a4083312091a793f6b32fcc35211
강윤서 <ysys1130@gmail.com>
2024-08-02 12:58:06 +0900

    feat: Global exception 설정

--- backend/src/main/java/com/plog/backend/global/exception/model/ExceptionResponseDto.java
+++ backend/src/main/java/com/plog/backend/global/exception/model/ExceptionResponseDto.java
@@ -1,18 +1,18 @@
-package com.plog.backend.global.exception;
+package com.plog.backend.global.exception.model;
 
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-public class ExceptionResponse {
+public class ExceptionResponseDto {
     private String httpMethod;
     private String requestURL;
     private int httpStatus;
     private String message;
     private LocalDateTime timestamp;
 
-    public ExceptionResponse(String httpMethod, String requestURL, int httpStatus, String message, LocalDateTime timestamp) {
+    public ExceptionResponseDto(String httpMethod, String requestURL, int httpStatus, String message, LocalDateTime timestamp) {
         this.httpMethod = httpMethod;
         this.requestURL = requestURL;
         this.httpStatus = httpStatus;
@@ -20,7 +20,7 @@ public class ExceptionResponse {
         this.timestamp = timestamp;
     }
 
-    public static ExceptionResponse of(String httpMethod, String requestURL, int httpStatus, String message) {
-        return new ExceptionResponse(httpMethod, requestURL, httpStatus, message, LocalDateTime.now());
+    public static ExceptionResponseDto of(String httpMethod, String requestURL, int httpStatus, String message) {
+        return new ExceptionResponseDto(httpMethod, requestURL, httpStatus, message, LocalDateTime.now());
     }
 }
