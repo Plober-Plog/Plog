046bfcfe69f0dfa096fe0165ac544c7ef1ce69e9
강윤서 <ysys1130@gmail.com>
2024-08-02 09:37:05 +0900

    chore: 식물 dummy data 삽입

--- backend/src/main/java/com/plog/backend/domain/plant/exception/NotValidPlantTypeIdsException.java
+++ backend/src/main/java/com/plog/backend/domain/plant/exception/NotValidPlantTypeIdsException.java
@@ -1,16 +1,2 @@
-package com.plog.backend.domain.plant.exception;
-
-import lombok.Getter;
-
-@Getter
-public class NotValidPlantTypeIdsException extends RuntimeException {
-    private final String message;
-
-    public NotValidPlantTypeIdsException() {
-        this.message = "유효한 식물 종류가 아닙니다.";
-    }
-
-    public NotValidPlantTypeIdsException(String message) {
-        this.message = message;
-    }
+package com.plog.backend.domain.plant.exception;public class NotValidPlantTypeIdsException {
 }
