87f1acc6676a2bb99c41b188609271ace04ffa37
강윤서 <ysys1130@gmail.com>
2024-08-03 22:47:35 +0900

    feat: 식물 정보, 종류, 일지 repository 설정

--- backend/src/main/java/com/plog/backend/domain/plant/repository/PlantCheckRepository.java
+++ backend/src/main/java/com/plog/backend/domain/plant/repository/PlantCheckRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PlantCheckRepository extends JpaRepository<PlantCheck, Long> {
+    Optional<List<PlantCheck>> findAllByPlantPlantId(Long plantId);
     Optional<PlantCheck> findByPlantPlantIdAndCheckDate(Long plantId, LocalDate checkDate);
 }
--- backend/src/main/java/com/plog/backend/domain/plant/repository/PlantTypeRepository.java
+++ backend/src/main/java/com/plog/backend/domain/plant/repository/PlantTypeRepository.java
@@ -2,6 +2,8 @@ package com.plog.backend.domain.plant.repository;
 
 import com.plog.backend.domain.plant.entity.PlantType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlantTypeRepository extends JpaRepository<PlantType, Long> {
 }
