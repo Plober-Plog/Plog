dfde1223f90b2a997cd2e686f188fbbde53e1971
강윤서 <ysys1130@gmail.com>
2024-08-05 00:48:34 +0900

    docs: exception 설명 주석 추가

--- backend/src/main/java/com/plog/backend/global/exception/NotValidRequestException.java
+++ backend/src/main/java/com/plog/backend/global/exception/NotValidRequestException.java
@@ -2,7 +2,7 @@ package com.plog.backend.global.exception;
 
 import lombok.Getter;
 
-// TODO [강윤서] 어떤 exception을 생각하고 만들었는지 써주기
+// 요청이 유효하지 않을 경우의 예외
 @Getter
 public class NotValidRequestException extends RuntimeException {
     private final String message;
