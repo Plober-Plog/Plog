2604dde1ba59f25adc07638994550419e1c06c48
gapple95 <gapple95@naver.com>
2024-08-05 10:05:16 +0900

    fix: JWT 토큰 로그인 정책 추가

--- backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/user").authenticated() // 회원 정보 조회 JWT 필요
                         .requestMatchers(HttpMethod.GET, "/api/user/plant/**/share").authenticated() // 식물 프로필 공유 JWT 필요
                         .requestMatchers(HttpMethod.GET, "/api/user/sns/**/comment").authenticated() // 댓글 조회 JWT 필요
+                        .requestMatchers(HttpMethod.POST, "/api/user/login").permitAll() // 로그인 JWT 없이 접근 가능
                         .requestMatchers(HttpMethod.POST,"/api/user/password").authenticated() // 현재 비밀번호 확인 JWT 필요
                         .requestMatchers(HttpMethod.POST,"/api/user/plant/**").authenticated() // 식물 등록 JWT 필요
                         .requestMatchers(HttpMethod.POST,"/api/user/diary/**").authenticated() // 식물 일지 등록 JWT 필요
