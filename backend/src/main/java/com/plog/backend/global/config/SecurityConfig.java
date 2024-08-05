40e68f57f0f4503fe2858ba22485ce68b25c26fd
gapple95 <gapple95@naver.com>
2024-08-05 10:29:49 +0900

    fix: 권한 해제

--- backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
@@ -27,18 +27,19 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/admin/**").authenticated() // JWT 필요
-                        .requestMatchers(HttpMethod.PATCH,"/api/user/**").authenticated() // 수정 JWT 필요
-                        .requestMatchers(HttpMethod.DELETE,"/api/user/**").authenticated() // 삭제 JWT 필요
-                        .requestMatchers(HttpMethod.GET, "/api/user").authenticated() // 회원 정보 조회 JWT 필요
-                        .requestMatchers(HttpMethod.GET, "/api/user/plant/**/share").authenticated() // 식물 프로필 공유 JWT 필요
-                        .requestMatchers(HttpMethod.GET, "/api/user/sns/**/comment").authenticated() // 댓글 조회 JWT 필요
-                        .requestMatchers(HttpMethod.POST, "/api/user/login").permitAll() // 로그인 JWT 없이 접근 가능
-                        .requestMatchers(HttpMethod.POST,"/api/user/password").authenticated() // 현재 비밀번호 확인 JWT 필요
-                        .requestMatchers(HttpMethod.POST,"/api/user/plant/**").authenticated() // 식물 등록 JWT 필요
-                        .requestMatchers(HttpMethod.POST,"/api/user/diary/**").authenticated() // 식물 일지 등록 JWT 필요
-                        .requestMatchers("/api/user/**").permitAll() // JWT 없이 접근 가능
-                        .anyRequest().authenticated() // 나머지 요청은 JWT 필요
+                        .anyRequest().permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/user").permitAll() // 회원가입은 JWT 없이 접근 가능
+//                        .requestMatchers("/api/admin/**").authenticated() // JWT 필요
+//                        .requestMatchers(HttpMethod.PATCH, "/api/user/**").authenticated() // 수정 JWT 필요
+//                        .requestMatchers(HttpMethod.DELETE, "/api/user/**").authenticated() // 삭제 JWT 필요
+//                        .requestMatchers(HttpMethod.GET, "/api/user").authenticated() // 회원 정보 조회 JWT 필요
+//                        .requestMatchers(HttpMethod.GET, "/api/user/plant/**/share").authenticated() // 식물 프로필 공유 JWT 필요
+//                        .requestMatchers(HttpMethod.GET, "/api/user/sns/**/comment").authenticated() // 댓글 조회 JWT 필요
+//                        .requestMatchers(HttpMethod.POST, "/api/user/password").authenticated() // 현재 비밀번호 확인 JWT 필요
+//                        .requestMatchers(HttpMethod.POST, "/api/user/plant/**").authenticated() // 식물 등록 JWT 필요
+//                        .requestMatchers(HttpMethod.POST, "/api/user/diary/**").authenticated() // 식물 일지 등록 JWT 필요
+//                        .requestMatchers("/api/user/**").permitAll() // JWT 없이 접근 가능
+//                        .anyRequest().authenticated() // 나머지 요청은 JWT 필요
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
