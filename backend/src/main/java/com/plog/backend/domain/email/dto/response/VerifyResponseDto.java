7a0a1317eab479481416e390675b201085422c58
gapple95 <gapple95@naver.com>
2024-08-05 20:33:32 +0900

    fix: 이메일 인증 에러 수정

--- backend/src/main/java/com/plog/backend/domain/email/dto/response/VerifyResponseDto.java
+++ backend/src/main/java/com/plog/backend/domain/email/dto/response/VerifyResponseDto.java
@@ -1,12 +1,14 @@
 package com.plog.backend.domain.email.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class VerifyResponseDto {
     private boolean result;
     private String message;
