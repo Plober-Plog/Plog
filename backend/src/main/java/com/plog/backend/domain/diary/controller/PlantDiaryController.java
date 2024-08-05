e7a4cba8fa5ad34f60c9bcabb1002a13e06d332e
강윤서 <ysys1130@gmail.com>
2024-08-05 17:51:22 +0900

    fix: addPlantDiary request 형식 수정

--- backend/src/main/java/com/plog/backend/domain/diary/controller/PlantDiaryController.java
+++ backend/src/main/java/com/plog/backend/domain/diary/controller/PlantDiaryController.java
@@ -22,7 +22,8 @@ public class PlantDiaryController {
     @PostMapping
     public ResponseEntity<BaseResponseBody> addPlantDiary(
             @RequestHeader("Authorization") String token,
-            @RequestPart PlantDiaryAddRequestDto plantDiaryAddRequestDto) {
+            @ModelAttribute PlantDiaryAddRequestDto plantDiaryAddRequestDto,
+        @RequestPart List<PlantDiaryImageUploadRequestDto> images) {
         log.info(">>> [POST] /user/diary - 요청 데이터: {}", plantDiaryAddRequestDto);
         plantDiaryService.addPlantDiary(token, plantDiaryAddRequestDto);
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "식물 일지 등록이 완료되었습니다."));
