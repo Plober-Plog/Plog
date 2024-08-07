4c1c7d18de9cbee1daa7e3b46fdd127b2caa9c9a
dldudnjs198 <dlduddnjs198@naver.com>
2024-08-07 17:24:38 +0900

    feat: backend-realtime에 사용할 기본 설정
    1. backend에서 필요한 entity, controller, repository 설정 등을 가져와서 backend-realtime에 사용하기 위해 추가
    2. image는 user에 포함

---
+++ backend-realtime/src/main/java/com/plog/realtime/domain/user/entity/User.java
@@ -0,0 +1,84 @@
+package com.plog.realtime.domain.user.entity;
+
+import com.plog.realtime.domain.user.entity.Image;
+import com.plog.realtime.global.model.dto.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString(exclude = {"password","provider","providerId","image"})
+@Builder
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @OneToOne
+    @JoinColumn(name="image_id", referencedColumnName = "imageId")
+    private Image image;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String searchId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private LocalDate birthDate;
+
+//    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column
+    private String source;
+
+//    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private State state;
+
+//    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+//    @Enumerated(EnumType.STRING)
+    @Column
+    private Provider provider;
+
+    @Column
+    private String providerId;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int totalExp;
+
+//    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ChatAuth chatAuth;
+
+    @Column
+    private String profileInfo;
+
+    @Column
+    @ColumnDefault("false")
+    private boolean isAd;
+
+    private int sidoCode;
+    private int gugunCode;
+}
---
+++ backend-realtime/src/main/java/com/plog/realtime/global/model/response/BaseResponseBody.java
@@ -0,0 +1,29 @@
+package com.plog.realtime.global.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseResponseBody {
+    String message = null;
+    Integer statusCode = null;
+
+    private BaseResponseBody() {}
+
+    private BaseResponseBody(Integer statusCode){
+        this.statusCode = statusCode;
+    }
+
+    private BaseResponseBody(Integer statusCode, String message){
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public static BaseResponseBody of(Integer statusCode, String message) {
+        BaseResponseBody body = new BaseResponseBody();
+        body.message = message;
+        body.statusCode = statusCode;
+        return body;
+    }
+}
