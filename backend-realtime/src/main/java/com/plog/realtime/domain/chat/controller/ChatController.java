a7f6d677c62c1ea88e02d64dcb9e613a32a54416
강윤서 <ysys1130@gmail.com>
2024-08-09 01:13:00 +0900

    feat: chat controller 추가

--- backend-realtime/src/main/java/com/plog/realtime/domain/chat/controller/ChatController.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/chat/controller/ChatController.java
@@ -1,18 +1,18 @@
 package com.plog.realtime.domain.chat.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plog.realtime.domain.chat.dto.request.ChatGetRequestDto;
 import com.plog.realtime.domain.chat.dto.response.ChatGetResponseDto;
-import com.plog.realtime.global.util.RedisMessagePublisher;
+import com.plog.realtime.domain.chat.service.ChatService;
+import com.plog.realtime.global.exception.NotValidRequestException;
+import com.plog.realtime.global.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -20,38 +20,73 @@ import java.util.List;
 @RestController
 @RequestMapping("/user/chat")
 public class ChatController {
-    private final RedisMessagePublisher redisMessagePublisher;
-    private final ObjectMapper objectMapper;
-    private final StringRedisTemplate redisTemplate;
 
-    @MessageMapping("/chat.sendMessage")
-    public void sendMessage(ChatGetResponseDto chatMessage) throws JsonProcessingException {
-        log.info(" >>> sendMessage: " + chatMessage);
-        chatMessage.setCreatedAt(LocalDateTime.now());
-        String jsonMessage = objectMapper.writeValueAsString(chatMessage);
-        redisMessagePublisher.publish(chatMessage);
+    private final ChatService chatService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @MessageMapping("/chat.sendMessage/{chatRoomId}")
+    public void sendMessage(
+            @DestinationVariable("chatRoomId") String chatRoomId,
+            ChatGetRequestDto chatGetRequestDto,
+            StompHeaderAccessor headerAccessor
+    ) {
+        String token = headerAccessor.getFirstNativeHeader("Authorization");
+        if (token == null) {
+            throw new NotValidRequestException("토큰은 필수값입니다.");
+        }
+        Long userId = JwtTokenUtil.jwtTokenUtil.getUserIdFromToken(token);
+        chatGetRequestDto.setUserId(userId);
+        chatGetRequestDto.setChatRoomId(Long.parseLong(chatRoomId));
+        log.info(" >>> sendMessage: " + chatGetRequestDto);
+        chatService.sendMessage(chatGetRequestDto);
     }
 
-    @MessageMapping("/chat.addUser")
-    public void addUser(ChatGetResponseDto chatMessage) {
-        log.info(" >>> addUser: " + chatMessage);
-        chatMessage.setMessage(chatMessage.getNickname() + " joined");
-        chatMessage.setCreatedAt(LocalDateTime.now());
-        redisMessagePublisher.publish(chatMessage);
+    @MessageMapping("/chat.addUser/{chatRoomId}")
+    public void addUser(
+            @DestinationVariable("chatRoomId") String chatRoomId,
+            ChatGetRequestDto chatGetRequestDto,
+            StompHeaderAccessor headerAccessor
+    ) {
+        String token = headerAccessor.getFirstNativeHeader("Authorization");
+        if (token == null) {
+            throw new NotValidRequestException("토큰은 필수값입니다.");
+        }
+        Long userId = JwtTokenUtil.jwtTokenUtil.getUserIdFromToken(token);
+        chatGetRequestDto.setUserId(userId);
+        chatGetRequestDto.setChatRoomId(Long.parseLong(chatRoomId));
+        log.info(" >>> addUser: " + chatGetRequestDto);
+        chatService.addUser(chatGetRequestDto);
     }
 
-    @MessageMapping("/chat.leaveUser")
-    public void leaveUser(ChatGetResponseDto chatMessage) {
-        log.info(" >>> leaveUser: " + chatMessage);
-        chatMessage.setMessage(chatMessage.getNickname() + " leaved");
-        chatMessage.setCreatedAt(LocalDateTime.now());
-        redisMessagePublisher.publish(chatMessage);
+    @MessageMapping("/chat.leaveUser/{chatRoomId}")
+    public void leaveUser(
+            @DestinationVariable("chatRoomId") String chatRoomId,
+            ChatGetRequestDto chatGetRequestDto,
+            StompHeaderAccessor headerAccessor
+    ) {
+        String token = headerAccessor.getFirstNativeHeader("Authorization");
+        if (token == null) {
+            throw new NotValidRequestException("토큰은 필수값입니다.");
+        }
+        Long userId = JwtTokenUtil.jwtTokenUtil.getUserIdFromToken(token);
+        chatGetRequestDto.setUserId(userId);
+        chatGetRequestDto.setChatRoomId(Long.parseLong(chatRoomId));
+        log.info(" >>> leaveUser: " + chatGetRequestDto);
+        chatService.leaveUser(chatGetRequestDto);
     }
 
-//    // Redis에 저장된 메시지를 확인하는 엔드포인트
-//    @GetMapping("/chat/messages")
-//    public List<String> getMessages() {
-//        return redisTemplate.opsForList().range("chatroom", 0, -1);
-//    }
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<List<ChatGetResponseDto>> getChatData(
+            @RequestHeader("Authorization") String token,
+            @PathVariable("chatRoomId") Long chatRoomId,
+            @RequestParam(required = false, defaultValue = "0") int page
+    ) {
+        if (token == null) {
+            throw new NotValidRequestException("토큰은 필수값입니다.");
+        }
+        log.info(" >>> getChatData: " + chatRoomId);
+        Long userId = jwtTokenUtil.getUserIdFromToken(token);
+        List<ChatGetResponseDto> chatGetResponseDtoList = chatService.getChatData(userId, chatRoomId, page);
+        return ResponseEntity.status(200).body(chatGetResponseDtoList);
+    }
 }
-
