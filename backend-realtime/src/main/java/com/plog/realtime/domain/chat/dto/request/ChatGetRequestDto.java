cccde5410d5e7f55f1bfc69d30317e55a1d1f313
강윤서 <ysys1130@gmail.com>
2024-08-08 21:08:59 +0900

    feat: chat dto 설정

--- backend-realtime/src/main/java/com/plog/realtime/domain/chat/dto/request/ChatGetRequestDto.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/chat/dto/request/ChatGetRequestDto.java
@@ -1,11 +1,19 @@
 package com.plog.realtime.domain.chat.dto.request;
 
-import java.time.LocalDateTime;
+import com.plog.realtime.domain.chat.entity.ChatType;
+import lombok.*;
 
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
 public class ChatGetRequestDto {
     String nickname;
     String profile;
     String message;
-    int chatType;
+    ChatType chatType;
     LocalDateTime createdAt;
 }
