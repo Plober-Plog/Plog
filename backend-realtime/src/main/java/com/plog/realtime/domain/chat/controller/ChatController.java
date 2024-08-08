e56289895f7290a4d720ca7d72fe2291e5877906
강윤서 <ysys1130@gmail.com>
2024-08-08 21:09:38 +0900

    feat: chat controller 추가

--- backend-realtime/src/main/java/com/plog/realtime/domain/chat/controller/ChatController.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/chat/controller/ChatController.java
@@ -1,17 +1,57 @@
 package com.plog.realtime.domain.chat.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plog.realtime.domain.chat.dto.response.ChatGetResponseDto;
+import com.plog.realtime.global.util.RedisMessagePublisher;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/user/chat")
 public class ChatController {
-    // post -> 채팅방 들어가기
+    private final RedisMessagePublisher redisMessagePublisher;
+    private final ObjectMapper objectMapper;
+    private final StringRedisTemplate redisTemplate;
 
-    // post -> 메시지 보내기
+    @MessageMapping("/chat.sendMessage")
+    public void sendMessage(ChatGetResponseDto chatMessage) throws JsonProcessingException {
+        log.info(" >>> sendMessage: " + chatMessage);
+        chatMessage.setCreatedAt(LocalDateTime.now());
+        String jsonMessage = objectMapper.writeValueAsString(chatMessage);
+        redisMessagePublisher.publish(chatMessage);
+    }
 
-    // get -> 메시지 받기
-    
+    @MessageMapping("/chat.addUser")
+    public void addUser(ChatGetResponseDto chatMessage) {
+        log.info(" >>> addUser: " + chatMessage);
+        chatMessage.setMessage(chatMessage.getNickname() + " joined");
+        chatMessage.setCreatedAt(LocalDateTime.now());
+        redisMessagePublisher.publish(chatMessage);
+    }
+
+    @MessageMapping("/chat.leaveUser")
+    public void leaveUser(ChatGetResponseDto chatMessage) {
+        log.info(" >>> leaveUser: " + chatMessage);
+        chatMessage.setMessage(chatMessage.getNickname() + " leaved");
+        chatMessage.setCreatedAt(LocalDateTime.now());
+        redisMessagePublisher.publish(chatMessage);
+    }
+
+//    // Redis에 저장된 메시지를 확인하는 엔드포인트
+//    @GetMapping("/chat/messages")
+//    public List<String> getMessages() {
+//        return redisTemplate.opsForList().range("chatroom", 0, -1);
+//    }
 }
+
--- backend-realtime/src/main/java/com/plog/realtime/domain/chat/service/ChatServiceImpl.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/chat/service/ChatServiceImpl.java
@@ -1,4 +1,30 @@
 package com.plog.realtime.domain.chat.service;
 
-public class ChatServiceImpl {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plog.realtime.domain.chat.dto.request.ChatGetRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketMessage;
+
+@RequiredArgsConstructor
+@Service("chatService")
+public class ChatServiceImpl implements ChatService {
+    private final StringRedisTemplate redisTemplate;
+    private final ObjectMapper objectMapper;
+
+
+    public void publishMessage(String channel, String message) {
+        redisTemplate.convertAndSend(channel, message);
+    }
+
+    public void saveMessage(ChatGetRequestDto chatMessage) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(chatMessage);
+            redisTemplate.opsForList().rightPush("chatMessages", jsonMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace(); // 예외 처리 로직을 추가할 수 있습니다.
+        }
+    }
 }
