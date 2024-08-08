eb1c2aa0cdb11bc8abfbac5e61a072af1eb48b03
이영원 <dlduddnjs198@naver.com>
2024-08-08 22:05:24 +0900

    refactor: Update FCMServiceImpl.java

--- backend-realtime/src/main/java/com/plog/realtime/domain/notification/service/FCMServiceImpl.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/notification/service/FCMServiceImpl.java
@@ -2,6 +2,7 @@ package com.plog.realtime.domain.notification.service;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
