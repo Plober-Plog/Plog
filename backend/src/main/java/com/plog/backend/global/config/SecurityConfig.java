f64b0ecdd29ecba3053ba4d020dce3eb8595440f
gapple95 <gapple95@naver.com>
2024-08-08 21:29:33 +0900

    securityConfig 수정

--- backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/plog/backend/global/config/SecurityConfig.java
@@ -47,18 +47,18 @@ public class SecurityConfig {
 //                        .requestMatchers("/api/user/**").permitAll() // JWT 없이 접근 가능
 //                        .anyRequest().authenticated() // 나머지 요청은 JWT 필요
                 )
-                .headers(headers -> headers
-                        .addHeaderWriter((request, response) -> {
-                            response.addHeader("Access-Control-Allow-Origin", "*");
-                            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                            response.addHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token, accessToken, refreshToken, userData");
-                            response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
-                            response.addHeader("Access-Control-Allow-Credentials", "true");
-                            if (request.getMethod().equals("OPTIONS")) {
-                                response.setStatus(HttpServletResponse.SC_OK);
-                            }
-                        })
-                )
+//                .headers(headers -> headers
+//                        .addHeaderWriter((request, response) -> {
+//                            response.addHeader("Access-Control-Allow-Origin", "*");
+//                            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//                            response.addHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token, accessToken, refreshToken, userData");
+//                            response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+//                            response.addHeader("Access-Control-Allow-Credentials", "true");
+//                            if (request.getMethod().equals("OPTIONS")) {
+//                                response.setStatus(HttpServletResponse.SC_OK);
+//                            }
+//                        })
+//                )
 
 //                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
