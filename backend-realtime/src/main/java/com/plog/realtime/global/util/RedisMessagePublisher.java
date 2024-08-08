a09b4edbdd91d2fc36b1488c184d792d8f9a9872
강윤서 <ysys1130@gmail.com>
2024-08-09 01:13:29 +0900

    fix: 매개변수 type 변경

--- backend-realtime/src/main/java/com/plog/realtime/global/util/RedisMessagePublisher.java
+++ backend-realtime/src/main/java/com/plog/realtime/global/util/RedisMessagePublisher.java
@@ -1,5 +1,6 @@
 package com.plog.realtime.global.util;
 
+import com.plog.realtime.domain.chat.dto.request.ChatGetRequestDto;
 import com.plog.realtime.domain.chat.dto.response.ChatGetResponseDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -14,7 +15,7 @@ public class RedisMessagePublisher {
         this.topic = topic;
     }
 
-    public void publish(ChatGetResponseDto message) {
+    public void publish(ChatGetRequestDto message) {
         redisTemplate.convertAndSend(topic.getTopic(), message);
 //        redisTemplate.opsForList().rightPush("chatroom", message); // 메시지를 Redis 리스트에 저장
     }
