0a3ba649259958db32f0335ef5822b132ba0b4de
강윤서 <ysys1130@gmail.com>
2024-08-08 21:09:25 +0900

    feat: redis pub, sub 추가

--- backend-realtime/src/main/java/com/plog/realtime/global/util/RedisMessagePublisher.java
+++ backend-realtime/src/main/java/com/plog/realtime/global/util/RedisMessagePublisher.java
@@ -1,2 +1,21 @@
-package com.plog.realtime.global.util;public class RedisMessagePublisher {
+package com.plog.realtime.global.util;
+
+import com.plog.realtime.domain.chat.dto.response.ChatGetResponseDto;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+
+public class RedisMessagePublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ChannelTopic topic;
+
+    public RedisMessagePublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic topic) {
+        this.redisTemplate = redisTemplate;
+        this.topic = topic;
+    }
+
+    public void publish(ChatGetResponseDto message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+//        redisTemplate.opsForList().rightPush("chatroom", message); // 메시지를 Redis 리스트에 저장
+    }
 }
--- backend-realtime/src/main/java/com/plog/realtime/global/util/RedisMessageSubscriber.java
+++ backend-realtime/src/main/java/com/plog/realtime/global/util/RedisMessageSubscriber.java
@@ -1,2 +1,17 @@
-package com.plog.realtime.global.util;public class RedisMessageSubscriber {
+package com.plog.realtime.global.util;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+public class RedisMessageSubscriber {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public RedisMessageSubscriber(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void onMessage(String message, String channel) {
+        System.out.println("Received Message: " + message + " from Channel: " + channel);
+        messagingTemplate.convertAndSend("/topic/public", message);
+    }
 }
