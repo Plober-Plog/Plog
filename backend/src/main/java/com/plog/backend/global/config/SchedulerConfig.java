44eb2d91ef35e1fcc15bfb17b8b5bf46b00820ae
이영원 <dlduddnjs198@naver.com>
2024-08-09 22:47:38 +0900

    refactor: Update SchedulerConfig.java for test

--- backend/src/main/java/com/plog/backend/global/config/SchedulerConfig.java
+++ backend/src/main/java/com/plog/backend/global/config/SchedulerConfig.java
@@ -18,9 +18,9 @@ public class SchedulerConfig {
 
     private final WeatherService weatherService;
 
-    @Scheduled(cron = "0 48 0 * * ?")
+    @Scheduled(cron = "0 55 0 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate0() {
         try {
             log.info("내가 바로 00시다.");
             weatherService.updateWeatherData();
@@ -30,9 +30,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 1 * * ?")
+    @Scheduled(cron = "0 55 1 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate1() {
         try {
             log.info("내가 바로 01시다.");
             weatherService.updateWeatherData();
@@ -42,9 +42,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 2 * * ?")
+    @Scheduled(cron = "0 55 2 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate2() {
         try {
             log.info("내가 바로 02시다.");
             weatherService.updateWeatherData();
@@ -54,9 +54,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 3 * * ?")
+    @Scheduled(cron = "0 55 3 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate3() {
         try {
             log.info("내가 바로 03시다.");
             weatherService.updateWeatherData();
@@ -66,9 +66,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 4 * * ?")
+    @Scheduled(cron = "0 55 4 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate4() {
         try {
             log.info("내가 바로 04시다.");
             weatherService.updateWeatherData();
@@ -78,9 +78,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 5 * * ?")
+    @Scheduled(cron = "0 55 5 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate5() {
         try {
             log.info("내가 바로 05시다.");
             weatherService.updateWeatherData();
@@ -90,9 +90,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 6 * * ?")
+    @Scheduled(cron = "0 55 6 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate6() {
         try {
             log.info("내가 바로 06시다.");
             weatherService.updateWeatherData();
@@ -102,9 +102,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 7 * * ?")
+    @Scheduled(cron = "0 55 7 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate7() {
         try {
             log.info("내가 바로 07시다.");
             weatherService.updateWeatherData();
@@ -114,9 +114,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 8 * * ?")
+    @Scheduled(cron = "0 55 8 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate8() {
         try {
             log.info("내가 바로 08시다.");
             weatherService.updateWeatherData();
@@ -126,9 +126,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 9 * * ?")
+    @Scheduled(cron = "0 55 9 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate9() {
         try {
             log.info("내가 바로 09시다.");
             weatherService.updateWeatherData();
@@ -138,9 +138,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 10 * * ?")
+    @Scheduled(cron = "0 55 10 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate10() {
         try {
             log.info("내가 바로 10시다.");
             weatherService.updateWeatherData();
@@ -150,9 +150,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 11 * * ?")
+    @Scheduled(cron = "0 55 11 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate11() {
         try {
             log.info("내가 바로 11시다.");
             weatherService.updateWeatherData();
@@ -162,9 +162,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 12 * * ?")
+    @Scheduled(cron = "0 55 12 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate12() {
         try {
             log.info("내가 바로 12시다.");
             weatherService.updateWeatherData();
@@ -174,9 +174,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 13 * * ?")
+    @Scheduled(cron = "0 55 13 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate13() {
         try {
             log.info("내가 바로 13시다.");
             weatherService.updateWeatherData();
@@ -186,9 +186,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 14 * * ?")
+    @Scheduled(cron = "0 55 14 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate14() {
         try {
             log.info("내가 바로 14시다.");
             weatherService.updateWeatherData();
@@ -198,9 +198,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 15 * * ?")
+    @Scheduled(cron = "0 55 15 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate15() {
         try {
             log.info("내가 바로 15시다.");
             weatherService.updateWeatherData();
@@ -210,9 +210,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 16 * * ?")
+    @Scheduled(cron = "0 55 16 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate16() {
         try {
             log.info("내가 바로 16시다.");
             weatherService.updateWeatherData();
@@ -222,9 +222,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 17 * * ?")
+    @Scheduled(cron = "0 55 17 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate17() {
         try {
             log.info("내가 바로 17시다.");
             weatherService.updateWeatherData();
@@ -234,9 +234,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 18 * * ?")
+    @Scheduled(cron = "0 55 18 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate18() {
         try {
             log.info("내가 바로 18시다.");
             weatherService.updateWeatherData();
@@ -246,9 +246,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 19 * * ?")
+    @Scheduled(cron = "0 55 19 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate19() {
         try {
             log.info("내가 바로 19시다.");
             weatherService.updateWeatherData();
@@ -258,9 +258,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 20 * * ?")
+    @Scheduled(cron = "0 55 20 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate20() {
         try {
             log.info("내가 바로 20시다.");
             weatherService.updateWeatherData();
@@ -270,9 +270,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 21 * * ?")
+    @Scheduled(cron = "0 55 21 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate21() {
         try {
             log.info("내가 바로 21시다.");
             weatherService.updateWeatherData();
@@ -282,9 +282,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 22 * * ?")
+    @Scheduled(cron = "0 55 22 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate22() {
         try {
             log.info("내가 바로 22시다.");
             weatherService.updateWeatherData();
@@ -294,9 +294,9 @@ public class SchedulerConfig {
         }
     }
 
-    @Scheduled(cron = "0 48 23 * * ?")
+    @Scheduled(cron = "0 55 23 * * ?")
     @Retryable(value = Exception.class, maxAttempts = Integer.MAX_VALUE, backoff = @Backoff(delay = 60000))
-    public void scheduleWeatherDataUpdate() {
+    public void scheduleWeatherDataUpdate23() {
         try {
             log.info("내가 바로 23시다.");
             weatherService.updateWeatherData();
