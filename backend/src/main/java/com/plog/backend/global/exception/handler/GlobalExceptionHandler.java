0a8756013b54f97e5c6ae267da566d0a6a71a680
강윤서 <ysys1130@gmail.com>
2024-08-11 00:02:33 +0900

    fix: NoTokenRequestException http status 수정

--- backend/src/main/java/com/plog/backend/global/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/plog/backend/global/exception/handler/GlobalExceptionHandler.java
@@ -103,6 +103,6 @@ public class GlobalExceptionHandler {
                 HttpStatus.UNAUTHORIZED.value(),
                 ex.getMessage()
         );
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 }
