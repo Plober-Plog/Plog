2d79e3ad6f4cd50128b231241aa39574e27556b6
이영원 <dlduddnjs198@naver.com>
2024-08-12 13:05:42 +0900

    refactor: Update ProxyController.java

--- backend/src/main/java/com/plog/backend/domain/image/controller/ProxyController.java
+++ backend/src/main/java/com/plog/backend/domain/image/controller/ProxyController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/image")
+@Slf4j
 public class ProxyController {
 
     // S3 이미지 캡처를 위한 프록시 컨트롤러
@@ -18,11 +20,21 @@ public class ProxyController {
     public ResponseEntity<byte[]> getImage(@RequestParam String url) {
         RestTemplate restTemplate = new RestTemplate();
         try {
+            log.info("Attempting to fetch image from URL: {}", url);
+
             byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Content-Type", "image/jpeg");  // 이미지 타입에 맞게 변경
-            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+
+            if (imageBytes != null && imageBytes.length > 0) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Content-Type", "image/jpeg");  // 이미지 타입에 맞게 변경
+                log.info("Image fetched successfully from URL: {}", url);
+                return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+            } else {
+                log.error("Failed to fetch image: Received empty response from URL: {}", url);
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         } catch (Exception e) {
+            log.error("Error occurred while fetching image from URL: {}", url, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
