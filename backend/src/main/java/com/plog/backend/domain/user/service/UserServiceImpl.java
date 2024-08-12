ce6d220996ba0abbd279fdbc9c87ae6d4b52df2a
gapple95 <gapple95@naver.com>
2024-08-12 10:30:06 +0900

    fix: 로그아웃 완

--- backend/src/main/java/com/plog/backend/domain/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/plog/backend/domain/user/service/UserServiceImpl.java
@@ -142,10 +142,8 @@ public class UserServiceImpl implements UserService {
 
         Long userId = jwtTokenUtil.getUserIdFromToken(token);
 
-        String searchId = userRepository.findSearchIdByUserId(userId);
-
         // FCM 토큰 null로 변경
-        User user = userRepository.findUserBySearchId(searchId)
+        User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         user.setNotificationToken(null); // FCM 토큰을 null로 변경
