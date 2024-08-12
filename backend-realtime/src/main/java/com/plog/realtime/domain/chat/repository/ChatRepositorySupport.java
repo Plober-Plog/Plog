7d4b49a91c3b05988e2c3320552873ec9414c5b3
gapple95 <gapple95@naver.com>
2024-08-12 17:48:58 +0900

    fix: 댓글 페이지네이션 수정

--- backend-realtime/src/main/java/com/plog/realtime/domain/chat/repository/ChatRepositorySupport.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/chat/repository/ChatRepositorySupport.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class ChatRepositorySupport extends QuerydslRepositorySupport {
@@ -17,14 +18,25 @@ public class ChatRepositorySupport extends QuerydslRepositorySupport {
         super(Chat.class);
         this.queryFactory = paQueryFactory;
     }
+
     public List<Chat> findChatsByChatRoomId(Long chatRoomId, int page) {
         QChat chat = QChat.chat;
 
-        return queryFactory.selectFrom(chat)
+        // 모든 채팅 메시지를 최신순으로 정렬한 후
+        List<Chat> sortedChats = queryFactory.selectFrom(chat)
                 .where(chat.chatRoom.chatRoomId.eq(chatRoomId))
                 .orderBy(chat.createdAt.asc())
-                .offset(page * size)
-                .limit(size)
                 .fetch();
+
+        // 정렬된 리스트에서 페이지네이션 적용
+        List<Chat> filteredArticleList = sortedChats.stream()
+                .sorted((a1, a2) -> {
+                        return a2.getCreatedAt().compareTo(a1.getCreatedAt());
+                })
+                .skip(page * size)
+                .limit(size)
+                .collect(Collectors.toList());
+
+        return filteredArticleList;
     }
 }
