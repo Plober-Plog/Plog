1eb988a686e205b1025bc0cdf64b7e6a02af4f3a
강윤서 <ysys1130@gmail.com>
2024-08-15 17:02:44 +0900

    fix: 채팅 내역 인덱스 에러 해결

--- backend-realtime/src/main/java/com/plog/realtime/domain/chat/repository/ChatRepositorySupport.java
+++ backend-realtime/src/main/java/com/plog/realtime/domain/chat/repository/ChatRepositorySupport.java
@@ -8,6 +8,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,11 +26,19 @@ public class ChatRepositorySupport extends QuerydslRepositorySupport {
 
         QChat chat = QChat.chat;
 
+        // 전체 채팅 개수 조회
+        long totalChats = queryFactory.selectFrom(chat)
+                .where(chat.chatRoom.chatRoomId.eq(chatRoomId))
+                .fetchCount();
+
+        // 페이지네이션 범위 확인
+        if (page * size >= totalChats) {
+            return new ArrayList<>(); // 범위를 초과하면 빈 리스트 반환
+        }
+
         // 최신순으로 정렬하고 페이지네이션을 적용하여 쿼리 실행
         List<Chat> paginatedChats = queryFactory.selectFrom(chat)
-                .where(
-                        chat.chatRoom.chatRoomId.eq(chatRoomId)
-                )
+                .where(chat.chatRoom.chatRoomId.eq(chatRoomId))
                 .orderBy(chat.createdAt.desc()) // 최신순으로 정렬
                 .offset(page * size)             // 페이지 시작점
                 .limit(size)                     // 페이지 크기만큼 데이터 가져오기
@@ -37,4 +46,5 @@ public class ChatRepositorySupport extends QuerydslRepositorySupport {
 
         return paginatedChats;
     }
+
 }
